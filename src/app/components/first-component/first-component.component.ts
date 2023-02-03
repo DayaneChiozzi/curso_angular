acd9095161bc77dd6a1f19007b6747dc9f81a280
Dayane Chiozzi <daychiozzi@gmail.com>
2023-02-03 16:18:33 -0300

    sparkles:feat:interpolacao de dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Dayane';
+  age: number = 33;
+  job = 'Programadora';
+  hobbie = ['ler', 'dancar', 'cantar', 'assistir series'];
+  car = {
+    name: 'C4 Lounge',
+    year: 2015,
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
